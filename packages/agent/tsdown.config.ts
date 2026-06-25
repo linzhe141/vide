5c2071bdda9253e003a29bd7547244d7bfb7931c
linzhe <linzhe141@qq.com>
2026-06-25 23:36:34 +0800

    refactor: use internal ts package

--- packages/agent/tsdown.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'tsdown'
-
-export default defineConfig({
-  entry: ['./src/index.ts', './src/event/channels.ts', './src/types.ts'],
-  dts: true,
-  sourcemap: true,
-  clean: false,
-})
